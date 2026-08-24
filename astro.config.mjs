d436cc3f43f1eb2672d4e2d3a3a64fcc9198390b
Jun <jun@rfs.jp>
2026-08-24 09:49:33 +0900

    tailwindに変換

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	server: {
 		host: true,
-		open: true
+		open: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
